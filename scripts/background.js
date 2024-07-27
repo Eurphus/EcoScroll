1bce166c47e0de4aa4eaa25a4c7c8d073225595a
Nathan Lu <155280155+NathanL15@users.noreply.github.com>
2024-07-27 14:51:25 -0400

    Format

--- scripts/background.js
+++ scripts/background.js
@@ -1,5 +1,18 @@
+let count = 0;
+
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
     if (changeInfo.status === 'complete' && tab.url && tab.url.includes('https://www.youtube.com/shorts/')) {
-        console.log('Contains');
+        count++;
+        console.log(count);
+
+        if (count === 10){
+            count = 0;
+
+            chrome.scripting.executeScript({
+                target:{tabId: tabId},
+                files:['content.js']
+            })
+
+        }
     }
 });
---
+++ scripts/content.js
@@ -0,0 +1,5 @@
+// Select the video element and pause it
+const video = document.querySelector('video');
+if (video) {
+    video.pause();
+}
