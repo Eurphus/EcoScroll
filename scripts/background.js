ed3a1c096a6ae11e89aa67b4eb5756aedf35975e
Nathan Lu <155280155+NathanL15@users.noreply.github.com>
2024-07-27 14:21:54 -0400

    Fixed Background

--- scripts/background.js
+++ scripts/background.js
@@ -1,7 +1,5 @@
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-    if (changeInfo === 'complete' && tab.url){
-        if (tab.url.includes('https://www.youtube.com/shorts/')){
-            console.log('Contains')
-        }
+    if (changeInfo.status === 'complete' && tab.url && tab.url.includes('https://www.youtube.com/shorts/')) {
+        console.log('Contains');
     }
-})
+});
