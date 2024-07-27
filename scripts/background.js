e942628167d42573f35aa05d49a6e55f126fef8a
Eurphus <p2macdon@uwaterloo.ca>
2024-07-27 17:41:47 -0400

    Pushing Update

--- scripts/background.js
+++ scripts/background.js
@@ -1,42 +1,83 @@
 let count = 0;
-let prev_url = ''
+let prev_url = '';
 let timerStarted = false;
+let intervalId;
+let timeElapsed = 0;
+let down_time = 0;
+let time_limit = 20;
+let count_limit = 5;
+let injected = false;
+let initial_run = true;
+let countdown = 60;
+let count_injected = false;
 
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
     if (changeInfo.status === 'complete' && tab.url && tab.url.includes('https://www.youtube.com/shorts/')) {
+        if (initial_run === true){
+            prev_url = tab.url;
+            initial_run = false;
+        }
         if (prev_url !== tab.url) {
             count++;
+
+            console.log('Contains', count);
         }
-        
-        console.log('Contains', count);
 
         if (!timerStarted) {
             timerStarted = true;
-            setTimeout(() => {
-                chrome.scripting.executeScript({
-                    target: { tabId: tabId },
-                    files: ['scripts/content.js']
-                }).then(() => {
-                    console.log('Content script injected after 20 seconds.');
-                }).catch((error) => {
-                    console.error('Error injecting content script:', error);
-                });
-            }, 20000); //20 second Timer
+
+
+            intervalId = setInterval(() => {
+                if (injected === false && count_injected === false){
+                    timeElapsed++;
+                    console.log(`Time elapsed: ${timeElapsed} seconds`);
+
+                    // inject after 20 secs
+                    if (timeElapsed >= time_limit) {
+                        chrome.scripting.executeScript({
+                            target: { tabId: tabId },
+                            files: ['scripts/content.js']
+                        }).then(() => {
+                            console.log('Content script injected after time limit reached.');
+                            injected = true
+                            down_time = 0;
+
+                        }).catch((error) => {
+                            console.error('Error injecting content script:', error);
+                            clearInterval(intervalId);
+                            timerStarted = false;
+                            timeElapsed = 0;
+                        });
+                    }
+                } else {
+                    down_time++;
+                    console.log(`Time down: ${down_time} seconds`);
+
+                }
+
+            }, 1000); //1 second interval
         }
 
-        // Count is double the limit since each short iterates twice.
-        if (count === 6) {
+        //    count should be double of the actual limit
+        if (count === count_limit) {
             count = 0;
             chrome.scripting.executeScript({
                 target: { tabId: tabId },
                 files: ['scripts/content.js']
             }).then(() => {
-                console.log('Content script injected after count reached 6.');
+                console.log('Content script injected after count limit reached');
+                count_injected = true;
+                down_time = 0;
+
             }).catch((error) => {
                 console.error('Error injecting content script:', error);
+                clearInterval(intervalId);
+                timerStarted = false;
+                timeElapsed = 0;
             });
         }
-        prev_url = tab.url
+
+        prev_url = tab.url;
     }
 });
 
